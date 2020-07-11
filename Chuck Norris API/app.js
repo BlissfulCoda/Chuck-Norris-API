103865894e2c671b1b4fe60b0cab7693c82bc8f0
Ronnie <43523873+BlissfulCoda@users.noreply.github.com>
2020-07-11 15:35:52 +0100

    using the fetch API & promises

--- Chuck Norris API/app.js
+++ Chuck Norris API/app.js
@@ -1,52 +1,30 @@
-const jokeButton = document.querySelector(".get-jokes");
+const buttonJokes = document.querySelector(".get-jokes");
 
-//jokebutton eventlistener
-jokeButton.addEventListener('click', getJokes);
+//ButtonJoke eventListener
+buttonJokes.addEventListener('click', fetchJokes);
 
-//getjokes function
-function getJokes(e){
-    //variable for selecting the input
+//Function FetchJokes
+function fetchJokes(e){
+    //input jokes from the user
     const numberOfJokes = document.querySelector('input[type="number"]').value;
     
-
-    //create an XHR Object
-    const xhr = new XMLHttpRequest();
-
-    //make an external asynchronous request from the chuck norris api
-    xhr.open("GET", `http://api.icndb.com/jokes/random/${numberOfJokes}`, true);
-
+    //Display jokes in the browser
+    const displayJokes = document.querySelector('.jokes')
     
-    xhr.onload = function(){
-        //check if the request status is OK
-        if(this.status === 200){
-
-            //parse all response jokes
-            const allJokes = JSON.parse(this.responseText);
-            
-            //created an empty string as a container for the jokes
-            let displayJokes = '';
-
-            //check if the joke status type returned is equal to success & if so, display the jokes
-            if(allJokes.type === 'success'){
-
-                //loop through the jokes value since it's returned as an array
-                allJokes.value.forEach(joke =>{
-                    displayJokes += `<li>${joke.joke}</li>`
-                });
-            } else {
-                //if the status jokes type is not 'success', then display this message
-                displayJokes = `<li> Sorry! No Jokes Found! </li>`
-            }
-
-            //grab the jokes class from index.html
-            const output = document.querySelector(".jokes");
-
-            //append the jokes and display them to the browser
-            output.innerHTML = displayJokes;
-        }
-    }
-
-    xhr.send();
+    //use the fetch API to collect the jokes
+    fetch(`http://api.icndb.com/jokes/random/${numberOfJokes}`)
+    //promise to collect the json data, which returns a promise
+    .then(res =>{
+        return res.json();
+    })
+    .then(jokes =>{
+        let output = '';
+        jokes.value.forEach(joke =>{
+            //append the new jokes to the previous joke
+            output += `<li> ${joke.joke}</li>`
+            displayJokes.innerHTML = output;
+        })
+    })
 
     e.preventDefault();
 }
